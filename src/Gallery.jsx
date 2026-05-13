e4b1ef8fed0f537bfeef9ff6cab5dca87ff483ee
agiv <webdev111222@gmail.com>
2026-05-13 15:47:17 -0700

    corrected hornedbeast

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,20 +1,10 @@
-import HornedBeast from './HornedBeast';
-
+import HornedBeasts from './HornedBeasts';
+import beast from '../data.json'
 
 function Gallery(props){
   return(<>
-<HornedBeast key={idx}
-            index={idx}
-            src={beast.image_url}
-            title={beast.title}
-            description={beast.description}
-            displayAsModal={props.displayAsModal}/>
-<HornedBeast key={idx}
-            index={idx}
-            src={beast.image_url}
-            title={beast.title}
-            description={beast.description}
-            displayAsModal={props.displayAsModal}/>
+<HornedBeasts title={'UniWhal'} imageUrl={'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'} description={'A unicorn and a narwhal nuzzling their horns'} />
+<HornedBeasts title={'Rhino Family'} imageUrl={'https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80'} description={'Parent rhino with two babies'} />
 </>
   )
 }
